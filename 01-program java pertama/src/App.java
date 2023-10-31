a7cd75efae3fd6b58dc89152a4dfff472b804b7e
maulana ihsan <146051538+Ichsan909@users.noreply.github.com>
2023-10-31 13:11:28 +0800

    31 oktober 2023
    program java pertama

---
+++ 01-program java pertama/src/App.java
@@ -0,0 +1,27 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // program biodata mahasiswa 
+        // deklarasi variable
+        String nama;
+        String nim;
+        String tgl_lahir;
+        String alamat;
+        float berat_badan;
+        float tinggi_badan;
+        // mengisi variable
+        nama = "Maulana ihsan";
+        nim = "23241049";
+        tgl_lahir = "31 desember 2002";
+        berat_badan = 50.f;
+        tinggi_badan = 175.5f;
+        // cetak Bioodata
+        System.out.println("==============");
+        System.out.println("nama : " + nama);
+        System.out.println("nim :" + nim);
+        System.out.println("tgl_lahir :" + tgl_lahir);
+        System.out.println("alamat :" + alamat);
+        System.out.println("berat_badan :" + berat_badan);
+        System.out.println("tinggi_badan :" + tinggi_badan);
+
+    }
+}
